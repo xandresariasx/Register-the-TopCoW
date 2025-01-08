5628365f6a49937adc50a65a6679efd77c86034c
xandresariasx <128938922+xandresariasx@users.noreply.github.com>
2025-01-08 17:04:55 +0100

    Update RegisterTopCow.bat

--- RegisterTopCow.bat
+++ RegisterTopCow.bat
@@ -34,6 +34,8 @@ if exist "%OutDirectory%" (
 )
 mkdir "%OutDirectory%"
 copy %ParameterFile1% %OutDirectory%
+copy %OutDirectory%Par0035.RIRE.MI.ri.ASGDPrime.txt %OutDirectory%InverseParameterFile.txt
+python %SoftwareFolder%Utilities\ChangeMetricParameterFile.py %OutDirectory%InverseParameterFile.txt
 
 :: Computation
 setlocal enabledelayedexpansion
@@ -41,14 +43,36 @@ for /L %%i in (1, 1, 165) do (
 	set "num=00%%i"
     set "num=!num:~-3!"
 	mkdir "%OutDirectory%Out!num!\"	
+	:: Register CT to MRI images and segmentations then compute dice
 	%SoftwareFolder%%ElastixFolder%elastix -f %DataFolder%\imagesTr\topcow_mr_!num!_0000.nii.gz -m %DataFolder%\imagesTr\topcow_ct_!num!_0000.nii.gz ^
 		-out %OutDirectory%Out!num!\ -p %ParameterFile1% 
+	move %OutDirectory%Out!num!\result.0.nii.gz %OutDirectory%Out!num!\CTmri.nii.gz	
+	copy %OutDirectory%Out!num!\TransformParameters.0.txt %OutDirectory%Out!num!\TransformParametersCTtoMRI.txt
 	%SoftwareFolder%%ElastixFolder%transformix -in %DataFolder%\cow_seg_labelsTr\topcow_ct_!num!.nii.gz -out %OutDirectory%Out!num!\ ^
-		-tp %OutDirectory%Out!num!\TransformParameters.0.txt	
+		-tp %OutDirectory%Out!num!\TransformParametersCTtoMRI.txt	
 	python %SoftwareFolder%Utilities\ReadNiiSegmentationAndBinarize.py %OutDirectory%Out!num!\result.nii.gz ^
 		--output %OutDirectory%Out!num!\SegmentationCTtoMRI.nii.gz
 	python %SoftwareFolder%Utilities\ReadNiiSegmentationAndBinarize.py %DataFolder%\cow_seg_labelsTr\topcow_mr_!num!.nii.gz ^
 		--output %OutDirectory%Out!num!\SegmentationMRI.nii.gz
 	python %SoftwareFolder%Utilities\ComputeDiceV2.py %OutDirectory%Out!num!\SegmentationCTtoMRI.nii.gz %OutDirectory%Out!num!\SegmentationMRI.nii.gz ^
-		%DataFolder%\roi_loc_labelsTr\topcow_mr_!num!.txt !num! %OutDirectory%Dices.txt
+		%DataFolder%\roi_loc_labelsTr\topcow_mr_!num!.txt !num! %OutDirectory%DicesMRI.txt
+	:: Inverse register MRI to CT images and segmentations then compute dice	
+	%SoftwareFolder%%ElastixFolder%elastix -f %DataFolder%\imagesTr\topcow_ct_!num!_0000.nii.gz -m %DataFolder%\imagesTr\topcow_ct_!num!_0000.nii.gz ^
+		-out %OutDirectory%Out!num!\ -t0 %OutDirectory%Out!num!\TransformParametersCTtoMRI.txt -p %OutDirectory%InverseParameterFile.txt		
+	copy %OutDirectory%Out!num!\TransformParameters.0.txt %OutDirectory%Out!num!\TransformParametersMRItoCT.txt
+	python %SoftwareFolder%Utilities\EditTransformationFile.py %OutDirectory%Out!num!\TransformParametersMRItoCT.txt ^
+		%OutDirectory%Out!num!\TransformParametersCTtoMRI.txt
+	%SoftwareFolder%%ElastixFolder%transformix -in %DataFolder%\imagesTr\topcow_mr_!num!_0000.nii.gz -out %OutDirectory%Out!num!\ ^
+		-tp %OutDirectory%Out!num!\TransformParametersMRItoCT.txt	
+	move %OutDirectory%Out!num!\result.nii.gz %OutDirectory%Out!num!\MRIct.nii.gz
+	copy %OutDirectory%Out!num!\TransformParameters.0.txt %OutDirectory%Out!num!\TransformParametersMRItoCTlr.txt	
+	python %SoftwareFolder%Utilities\EditTransformationFile2.py %OutDirectory%Out!num!\TransformParametersMRItoCTlr.txt 
+	%SoftwareFolder%%ElastixFolder%transformix -in %DataFolder%\cow_seg_labelsTr\topcow_mr_!num!.nii.gz -out %OutDirectory%Out!num!\ ^
+		-tp %OutDirectory%Out!num!\TransformParametersMRItoCTlr.txt	
+	python %SoftwareFolder%Utilities\ReadNiiSegmentationAndBinarize.py %OutDirectory%Out!num!\result.nii.gz ^
+		--output %OutDirectory%Out!num!\SegmentationMRItoCT.nii.gz	
+	python %SoftwareFolder%Utilities\ReadNiiSegmentationAndBinarize.py %DataFolder%\cow_seg_labelsTr\topcow_ct_!num!.nii.gz ^
+		--output %OutDirectory%Out!num!\SegmentationCT.nii.gz	
+	python %SoftwareFolder%Utilities\ComputeDiceV2.py %OutDirectory%Out!num!\SegmentationMRItoCT.nii.gz %OutDirectory%Out!num!\SegmentationCT.nii.gz ^
+		%DataFolder%\roi_loc_labelsTr\topcow_ct_!num!.txt !num! %OutDirectory%DicesCT.txt	
 )
